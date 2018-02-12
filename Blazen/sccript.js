5a56d1260d5c35c828f0d23ac6210faa59077ea2
Camyul <vankata.lt@outlook.com>
2018-02-12 12:11:20 +0200

    Check count of checkboxs

--- Blazen/sccript.js
+++ Blazen/sccript.js
@@ -1,30 +1,31 @@
-(function() {
+$(document).ready(function() {
     let ch = document.getElementsByClassName('pulsante_low');
 
     let radio = [];
+    if (ch.length > 0) {
+        for (let i = 0; i < ch.length; i += 1) {
 
-    for (let i = 0; i < ch.length; i += 1) {
+            if (ch[i].type == 'radio') {
+                radio.push(ch[i]);
+                continue;
+            }
 
-        if (ch[i].type == 'radio') {
-            radio.push(ch[i]);
-            continue;
+            ch[i].checked = true;
+
         }
 
-        ch[i].checked = true;
+        let randomNum = getRandomInt(1, 6);
 
+        radio[randomNum].checked = true;
+
+        setTimeout(complete, 50500);
     }
-
-    let randomNum = getRandomInt(1, 6);
 
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     };
 
-    radio[randomNum].checked = true;
-
-    setTimeout(complete, 50500);
-
     function complete() {
         document.getElementsByClassName('pulsante2')[0].click();
     }
-})();
+});
